a238e0a3a9e6a8e50f62ff27ffa9fbf63b1e3de1
yadubabu <mohini548@gmail.com>
2022-03-26 14:50:48 +0530

    This is REDUX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import Posts from './components/Posts';
+import PostForm from './components/Postform';
+
+
+import store from './store';
 
 function App() {
   return (
+   <Provider store={store}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='App-title'>Welcome to React</h1>
       </header>
+      <PostForm />
+      <hr/>
+      <Posts />
     </div>
+   </Provider>
   );
 }
 
